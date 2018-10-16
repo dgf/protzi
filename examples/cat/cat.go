7f9f835cb61c83f10c1278bd5f35b7e5f430b916
dgf <deep@dagnu.de>
2018-10-16 17:51:33 +0100

    behave like cat and flow only on arguments

--- examples/cat/cat.go
+++ examples/cat/cat.go
@@ -2,27 +2,49 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 
 	"github.com/dgf/protzi"
 	"github.com/dgf/protzi/component"
 )
 
+// go run cat.go
+// go run cat.go /etc/issue /unknown/file
+// echo -e "one\ntwo" | go run cat.go
+// echo -e "/etc/issue\n/unknown/file" | xargs go run cat.go
 func main() {
-	in := make(chan string)
-	net := protzi.New("cat")
 
+	// pipe or char device?
+	if info, err := os.Stdin.Stat(); err != nil {
+		panic(err)
+	} else if len(os.Args) == 1 && info.Mode()&(os.ModeNamedPipe|os.ModeCharDevice) != 0 {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+		return
+	}
+
+	// create network
+	net := protzi.New("cat")
 	net.Add("read", &component.TextFileRead{})
 	net.Add("output", &component.Output{})
 
+	// bind channels
+	in := make(chan string)
+	out := make(chan bool)
+	net.In("read.File", in)
+	net.Out("output.Printed", out)
+
+	// connect and run it
 	net.Connect("read.Text", "output.Message")
 	net.Connect("read.Error", "output.Message")
-
-	net.In("read.File", in)
 	net.Run()
 
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		in <- scanner.Text()
+	// flow the arguments
+	for _, arg := range os.Args[1:] {
+		in <- arg
+		<-out
 	}
 }
