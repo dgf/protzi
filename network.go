469f39659611275331c0ebc8f6dfeb023c239ac3
dgf <deep@dagnu.de>
2018-10-16 11:48:03 +0100

    unify validation chains

--- network.go
+++ network.go
@@ -15,6 +15,13 @@ type Network interface {
 	Run()
 }
 
+func convertibleTo(out, in reflect.Type) error {
+	if !in.ConvertibleTo(out) {
+		return fmt.Errorf("Types not convertible %s > %s\n", in, out)
+	}
+	return nil
+}
+
 type network struct {
 	name string
 
@@ -31,20 +38,9 @@ type network struct {
 	connections map[string]connection
 }
 
-// New creates a fresh empty network.
-func New(name string) Network {
-	return &network{
-		name:        name,
-		components:  map[string]Component{},
-		ins:         map[string]reflect.Value{},
-		outs:        map[string]reflect.Value{},
-		connections: map[string]connection{},
-	}
-}
-
 // Add a component with a unique name (initializes all unidirectional channels)
 func (n *network) Add(name string, c Component) {
-	log.Println("add", name)
+	log.Println("Add", name)
 	n.components[name] = c
 
 	// get underlying value and type
@@ -81,79 +77,78 @@ func (n *network) Add(name string, c Component) {
 
 // Connect two channels from the output of one to the input of another.
 func (n *network) Connect(out, in string) {
-	log.Println("connect", out, in)
+	cn := out + " > " + in
+	log.Println("Connect", cn)
 
 	if op, ok := n.outs[out]; !ok {
-		panic(fmt.Sprintf("input port %q not found", out))
+		panic(fmt.Sprintf("Input port %q not found", out))
 	} else if ip, ok := n.ins[in]; !ok {
-		panic(fmt.Sprintf("output port %q not found", in))
+		panic(fmt.Sprintf("Output port %q not found", in))
+	} else if _, ok := n.connections[cn]; ok {
+		panic(fmt.Sprintf("Connected %q before", cn))
+	} else if err := convertibleTo(ip.Type().Elem(), op.Type().Elem()); err != nil {
+		panic(err)
 	} else {
-		cn := out + " > " + in
-		if _, ok := n.connections[cn]; ok {
-			panic(fmt.Sprintf("connected %q before", cn))
-		}
-
-		outTypeElem := ip.Type().Elem()
-		inTypeElem := op.Type().Elem()
-		if !inTypeElem.ConvertibleTo(outTypeElem) {
-			panic(fmt.Sprintf("Type differs %s != %s\n", inTypeElem, outTypeElem))
-		}
-
 		n.connections[cn] = connection{name: cn, in: op, out: ip}
 	}
 }
 
 // Run starts the network by co-routine-ing all connection channels and components
 func (n *network) Run() {
-	log.Println("run net")
+	log.Println("Run network", n.name)
 	for _, c := range n.connections {
-		go func(c connection) { c.run() }(c)
+		go func(c connection) {
+			c.run()
+		}(c)
 	}
 	for _, c := range n.components {
-		go func(c Component) { c.Run() }(c)
+		go func(c Component) {
+			c.Run()
+		}(c)
 	}
 }
 
 // In maps an input channel
 func (n *network) In(in string, c interface{}) {
-	log.Println("in", in)
+	valueType := reflect.ValueOf(c).Type().Elem()
+	cn := "net > " + in
+	log.Println("In", cn, valueType)
+
 	if ip, ok := n.ins[in]; !ok {
-		panic(fmt.Sprintf("input port %q not found", in))
+		panic(fmt.Sprintf("Input port %q not found", in))
+	} else if _, ok := n.connections[cn]; ok {
+		panic(fmt.Sprintf("Connected %q before", cn))
+	} else if err := convertibleTo(ip.Type().Elem(), valueType); err != nil {
+		panic(err)
 	} else {
-		cn := "net > " + in
-		if _, ok := n.connections[cn]; ok {
-			panic(fmt.Sprintf("Connected %q before", cn))
-		}
-
-		outTypeElem := ip.Type().Elem()
-		valueOfChannel := reflect.ValueOf(c)
-		inTypeElem := valueOfChannel.Type().Elem()
-		if !inTypeElem.ConvertibleTo(outTypeElem) {
-			panic(fmt.Sprintf("Type differs %s > %s\n", inTypeElem, outTypeElem))
-		}
-
-		n.connections[cn] = connection{name: cn, in: valueOfChannel, out: ip}
+		n.connections[cn] = connection{name: cn, in: reflect.ValueOf(c), out: ip}
 	}
 }
 
 // Out maps an output channel
 func (n *network) Out(out string, c interface{}) {
-	log.Println("out", out)
+	valueType := reflect.ValueOf(c).Type().Elem()
+	cn := out + " > net"
+	log.Println("Out", cn, valueType)
+
 	if op, ok := n.outs[out]; !ok {
 		panic(fmt.Sprintf("output port %q not found", out))
+	} else if _, ok := n.connections[cn]; ok {
+		panic(fmt.Sprintf("connected %q before", cn))
+	} else if err := convertibleTo(valueType, op.Type().Elem()); err != nil {
+		panic(err)
 	} else {
-		cn := out + " > net"
-		if _, ok := n.connections[cn]; ok {
-			panic(fmt.Sprintf("connected %q before", cn))
-		}
-
-		inTypeElem := op.Type().Elem()
-		valueOfChannel := reflect.ValueOf(c)
-		outTypeElem := valueOfChannel.Type().Elem()
-		if !inTypeElem.ConvertibleTo(outTypeElem) {
-			panic(fmt.Sprintf("Type differs %s > %s\n", inTypeElem, outTypeElem))
-		}
-
 		n.connections[cn] = connection{name: cn, in: op, out: reflect.ValueOf(c)}
+	}
+}
+
+// New creates a fresh empty network.
+func New(name string) Network {
+	return &network{
+		name:        name,
+		components:  map[string]Component{},
+		ins:         map[string]reflect.Value{},
+		outs:        map[string]reflect.Value{},
+		connections: map[string]connection{},
 	}
 }
